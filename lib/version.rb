ed85bf30b6d38de72ee1cc0a74a2ff4eb6e4543a
Nathan Woodhull <woodhull@gmail.com>
2018-09-19 16:50:26 -0400

    initial commit

---
+++ lib/version.rb
@@ -0,0 +1,3 @@
+module NpgvanPublicEventListing
+  VERSION = '0.0.1'
+end
---
+++ spec/listing_spec.rb
@@ -0,0 +1,5 @@
+RSpec.describe NpgvanPublicEventListing do
+  it "has a version number" do
+    expect(NpgvanPublicEventListing::VERSION).not_to be nil
+  end
+end
